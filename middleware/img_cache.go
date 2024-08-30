84099ceca04bcad0824f5365b4143430e22d7751
lidong <hi_winter@163.com>
2024-08-30 09:48:48 +0800

    cache bugfix

--- middleware/img_cache.go
+++ middleware/img_cache.go
@@ -10,15 +10,14 @@ var ShootDateCacheMap map[string]string
 
 func CreateImgCache() {
 
+	ShootDateCacheMap = map[string]string{}
 	var imgDatabaseSearch model.ImgDatabaseSearch
 	list, _, err := imgDatabaseService.GetImgDatabaseInfoList(imgDatabaseSearch)
 	if err != nil {
 
 	}
 	for _, isd := range list {
-		if isd.ShootDate != "" {
-			ShootDateCacheMap[isd.ImgKey] = isd.ShootDate
-		}
+		ShootDateCacheMap[isd.ImgKey] = isd.ShootDate
 		/*if isd.LocAddr != "" {
 			GpsCacheMap[isd.ImgKey] = isd.LocAddr
 		}*/
