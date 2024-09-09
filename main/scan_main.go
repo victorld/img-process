0472164d774661f5bafcf4a98ddd64c74671fcc8
li_dong <li_dong@cestc.cn>
2024-09-09 11:29:32 +0800

    命令行执行不另外设置参数

--- main/scan_main.go
+++ main/scan_main.go
@@ -16,19 +16,7 @@ func main() {
 	cons.InitConst()
 	orm.InitMysql()
 
-	var startPath = ""
-	var startPathBak = ""
-
-	var deleteShow = true      //是否统计并显示非法文件和空目录
-	var moveFileShow = true    //是否统计并显示需要移动目录的文件
-	var modifyDateShow = false //是否统计并显示需要修改日期的文件
-	var md5Show = true         //是否统计并显示重复文件
-
-	var deleteAction = false     //是否操作删除非法文件和空目录
-	var moveFileAction = false   //是否操作需要移动目录的文件
-	var modifyDateAction = false //是否操作修改日期的文件
-
-	scanArgs := model.DoScanImgArg{DeleteShow: &deleteShow, MoveFileShow: &moveFileShow, ModifyDateShow: &modifyDateShow, Md5Show: &md5Show, DeleteAction: &deleteAction, MoveFileAction: &moveFileAction, ModifyDateAction: &modifyDateAction, StartPath: &startPath, StartPathBak: &startPathBak}
+	scanArgs := model.DoScanImgArg{DeleteShow: nil, MoveFileShow: nil, ModifyDateShow: nil, Md5Show: nil, DeleteAction: nil, MoveFileAction: nil, ModifyDateAction: nil, StartPath: nil, StartPathBak: nil}
 	tools.Logger.Info("DoScanImg main args : " + tools.MarshalJsonToString(scanArgs))
 
 	middleware.RegisterTable()
